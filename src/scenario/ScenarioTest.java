a4a83326f6bae966da7801b109d011209b3850eb
Jihanebgit <jihanebattas025@gmail.com>
2026-04-16 17:49:08 +0200

    suite tp

--- src/scenario/ScenarioTest.java
+++ src/scenario/ScenarioTest.java
@@ -1,5 +1,20 @@
 package scenario;
 
+import produit.Poisson;
+import produit.Produit;
+import produit.Sanglier;
+import villageGaulois.Etal;
+import villageGaulois.IEtal;
+
 public class ScenarioTest {
+
+	public static void main(String[] args) {
+		IEtal[] marche = new IEtal[3];
+		IEtal<Sanglier> etalSanglier = new Etal<>();
+		IEtal<Poisson> etalPoisson = new Etal<>();
+		marche[0] = etalSanglier;
+		marche[1] = etalPoisson;
+		IEtal<Produit>[]marche1= new IEtal[3];
+	}
 
 }
